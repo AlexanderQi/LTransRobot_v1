67dc350024c02a0d5ec4351238c283a5543bd0d5
Alexander Qi Li <VIP_liqi@hotmail.com>
2017-02-01 23:37:59 +0800

    1

---
+++ LTransRobot_v1/Control/uiSelectFile.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LTransRobot_v1.Control
+{
+    /// <summary>
+    /// uiSelectFile.xaml 的交互逻辑
+    /// </summary>
+    public partial class uiSelectFile : UserControl
+    {
+        public uiSelectFile()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LTransRobot_v1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using log4net;
+
+namespace LTransRobot_v1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ILog log; //log配置初始化在APP.XAML.CS中
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            log = LogManager.GetLogger("log");
+            log.Info("StartPath:" + AppDomain.CurrentDomain.SetupInformation.ApplicationBase);
+        }
+    }
+}
